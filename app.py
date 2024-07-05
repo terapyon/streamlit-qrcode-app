a1585939376ee9b18c4ac9b051e33088cd2fc471
Manabu TERADA <terada@cmscom.jp>
2024-07-05 20:13:14 +0900

    init app

---
+++ app.py
@@ -0,0 +1,18 @@
+from io import BytesIO
+import streamlit as st
+import qrcode
+
+st.title("デモアプリ")
+st.write("""# QRコード生成アプリ
+- ライブラリのインストール
+- 入力エリアを作る
+- QRコード変換プログラムを書く
+- QRコードを表示する
+""")
+
+text = st.text_input("入力エリア")
+
+with BytesIO() as out:
+    img = qrcode.make(text)
+    img.save(out)
+    st.image(out)
